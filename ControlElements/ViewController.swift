facb248ef5a5ab89a9f29249e3f3e1131c960c21
sumeyrademirtas <smyrdmrts@gmail.com>
2024-04-08 10:21:44 +0300

    Project Updated.

--- ControlElements/ViewController.swift
+++ ControlElements/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggle: UISwitch!
+    @IBOutlet var slider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func buttonTapped(_ button: UIButton){
+        print("Button was tapped!")
+        
+        if toggle.isOn{
+            print("Switch is on!")
+        }else{
+            print("Switch is off!")
+        }
+    }
 
+    @IBAction func switchToggled(_ sender: UISwitch){
+        if sender.isOn{
+            print("The switch is on!")
+        }else{
+            print("The switch is off!")
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ slider: UISlider){
+        print(slider.value)
+    }
+    
+    //texti değiştiğinde haberdar olmak gerekiyor, TextChanged
+    //Keyboeard üzerindeki return tuşu var. ona tıklanıp tıklanmadığını da bilmek lazım
+    @IBAction func keyboardReturnKeyTapped(_ textField: UITextField){
+        if let text = textField.text {
+            print(text)
+        }
+    }
+    
+    @IBAction func textChanged(_ textField: UITextField){
+        if let text = textField.text{
+            print(text)
+        }
+    }
+    
+    
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer){
+        let touch = sender.location(in: view)
+        view.endEditing(true)
+        print(touch)
+    }
 
 }
 
